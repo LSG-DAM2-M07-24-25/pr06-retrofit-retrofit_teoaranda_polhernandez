07e1d4d2847b08e23494f18bc5eb6d3b59519311
PelusinniDev <110785576+pelusinnidev@users.noreply.github.com>
2025-02-26 08:57:52 +0100

    Create TriviaApplication.kt

---
+++ app/src/main/java/com/lasalle/triviaLegends/TriviaApplication.kt
@@ -0,0 +1,13 @@
+package com.lasalle.triviaLegends
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * Classe principal de l'aplicació
+ * Configurada amb Hilt per la injecció de dependències
+ * 
+ * @author Pol & Teo
+ */
+@HiltAndroidApp
+class TriviaApplication : Application() 
